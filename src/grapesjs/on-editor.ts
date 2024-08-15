13ae2a604ba0f99f8a3137a0b33c5d829cd7477c
sheik-mostafizur <sheikmostafizur@gmail.com>
2024-08-15 18:34:42 +0600

    added simple testing component

--- src/grapesjs/on-editor.ts
+++ src/grapesjs/on-editor.ts
@@ -7,66 +7,15 @@ export default async (editor: Editor) => {
       return;
     }
 
-    // Function to log components
-    const logComponents = () => {
-      const components = editor.getComponents();
-      if (components) {
-        console.log("Components:", components.toJSON());
-      } else {
-        console.log("No components found");
-      }
-    };
+    const component = editor.addComponents(`<div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMTRFaiid-T0G4fqBsVi-Fqm69ciosB3opMQ&s" />
+      <span title="foo">Hello world!!!</span>
+    </div>`)[0];
 
-    // Attach event listeners to track changes
-    editor.on("component:add", logComponents);
-    editor.on("component:remove", logComponents);
-    editor.on("component:update", logComponents);
-    editor.on("style:update", logComponents);
-    editor.on("component:change", logComponents);
+    const innerComponents = component.components();
 
-    // Add a basic component to test functionality
-    editor.DomComponents.addType("test-component", {
-      model: {
-        defaults: {
-          testprop: 1,
-        },
-        init() {
-          console.log("Local hook: model.init");
-          this.listenTo(this, "change:testprop", this.handlePropChange);
-          // Here we can listen global hooks with editor.on('...')
-        },
-        updated(property, value, prevValue) {
-          console.log(
-            "Local hook: model.updated",
-            "property",
-            property,
-            "value",
-            value,
-            "prevValue",
-            prevValue
-          );
-        },
-        removed() {
-          console.log("Local hook: model.removed");
-        },
-        handlePropChange() {
-          console.log("The value of testprop", this.get("testprop"));
-        },
-      },
-      view: {
-        init() {
-          console.log("Local hook: view.init");
-        },
-        onRender() {
-          console.log("Local hook: view.onRender");
-        },
-      },
-    });
+    innerComponents.forEach((comp) => console.log(comp.toHTML()));
 
-    // A block for the custom component
-    editor.BlockManager.add("test-component", {
-      label: "Test Component",
-      content: '<div data-gjs-type="test-component">Test Component</div>',
-    });
+    console.log(innerComponents);
   }
 };
