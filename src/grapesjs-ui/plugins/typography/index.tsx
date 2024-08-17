b4b0451afa07097102cceae0c7f1b85e91b843bb
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-08-17 09:01:30 +0600

    Typography & Some Components added

--- src/grapesjs-ui/plugins/typography/index.tsx
+++ src/grapesjs-ui/plugins/typography/index.tsx
@@ -6,12 +6,116 @@ export const typographyPlugin = (editor: Editor) => {
   // Heading 1
   blockManager.add("typography-h1", {
     label: "Heading 1",
-    content: "<h1 class='bg-red-500 text-3xl'>Put your title here</h1>",
+    content:
+      "<h1 class='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>Heading 1</h1>",
     category: "Typography",
     attributes: {
       title: "Insert h1 block",
     },
     media:
       '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg"><path d="M448 96v320h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H320a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V288H160v128h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H32a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V96H32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16h-32v128h192V96h-32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16z"></path></svg>',
+  });
+
+  // Heading 2
+  blockManager.add("typography-h2", {
+    label: "Heading 2",
+    content:
+      "<h2 class='scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0'>Heading 2</h2>",
+    category: "Typography",
+    attributes: {
+      title: "Insert h2 block",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg"><path d="M448 96v320h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H320a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V288H160v128h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H32a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V96H32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16h-32v128h192V96h-32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16z"></path></svg>',
+  });
+
+  // Heading 3
+  blockManager.add("typography-h3", {
+    label: "Heading 3",
+    content:
+      "<h3 class='scroll-m-20 text-2xl font-semibold tracking-tight'>Heading 3</h3>",
+    category: "Typography",
+    attributes: {
+      title: "Insert h3 block",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg"><path d="M448 96v320h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H320a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V288H160v128h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H32a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V96H32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16h-32v128h192V96h-32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16z"></path></svg>',
+  });
+
+  // Heading 4
+  blockManager.add("typography-h4", {
+    label: "Heading 4",
+    content:
+      "<h4 class='scroll-m-20 text-xl font-semibold tracking-tight'>Heading 4</h4>",
+    category: "Typography",
+    attributes: {
+      title: "Insert h4 block",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg"><path d="M448 96v320h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H320a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V288H160v128h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H32a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V96H32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16h-32v128h192V96h-32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16z"></path></svg>',
+  });
+
+  // Heading 5
+  blockManager.add("typography-h5", {
+    label: "Heading 5",
+    content:
+      "<h5 class='scroll-m-20 text-lg font-semibold tracking-tight'>Heading 5</h5>",
+    category: "Typography",
+    attributes: {
+      title: "Insert h5 block",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg"><path d="M448 96v320h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H320a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V288H160v128h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H32a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V96H32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16h-32v128h192V96h-32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16z"></path></svg>',
+  });
+
+  // Heading 6
+  blockManager.add("typography-h6", {
+    label: "Heading 6",
+    content:
+      "<h6 class='scroll-m-20 text-base font-semibold tracking-tight'>Heading 5</h6>",
+    category: "Typography",
+    attributes: {
+      title: "Insert h6 block",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg"><path d="M448 96v320h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H320a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V288H160v128h32a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H32a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h32V96H32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16h-32v128h192V96h-32a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16z"></path></svg>',
+  });
+
+  // Paragraph
+  blockManager.add("typography-p", {
+    label: "Paragraph",
+    content: "<p class='leading-7 [&:not(:first-child)]:mt-6'>Paragraph</p>",
+    category: "Typography",
+    attributes: {
+      title: "Insert p block",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M448 48v32a16 16 0 0 1-16 16h-48v368a16 16 0 0 1-16 16h-32a16 16 0 0 1-16-16V96h-32v368a16 16 0 0 1-16 16h-32a16 16 0 0 1-16-16V352h-32a160 160 0 0 1 0-320h240a16 16 0 0 1 16 16z"></path></svg>',
+  });
+
+  // Blockquote
+  blockManager.add("typography-blockquote", {
+    label: "Blockquote",
+    content:
+      "<blockquote class='mt-6 border-l-2 pl-6 italic'>Blockquote</blockquote>",
+    category: "Typography",
+    attributes: {
+      title: "Insert blockquote element",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path></svg>',
+  });
+
+  // Code
+  blockManager.add("typography-code", {
+    label: "Code",
+    content:
+      "<code class='relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold'>@edust-org/frontend</code>",
+    category: "Typography",
+    attributes: {
+      title: "Insert code element",
+    },
+    media:
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg"><path d="M278.9 511.5l-61-17.7c-6.4-1.8-10-8.5-8.2-14.9L346.2 8.7c1.8-6.4 8.5-10 14.9-8.2l61 17.7c6.4 1.8 10 8.5 8.2 14.9L293.8 503.3c-1.9 6.4-8.5 10.1-14.9 8.2zm-114-112.2l43.5-46.4c4.6-4.9 4.3-12.7-.8-17.2L117 256l90.6-79.7c5.1-4.5 5.5-12.3.8-17.2l-43.5-46.4c-4.5-4.8-12.1-5.1-17-.5L3.8 247.2c-5.1 4.7-5.1 12.8 0 17.5l144.1 135.1c4.9 4.6 12.5 4.4 17-.5zm327.2.6l144.1-135.1c5.1-4.7 5.1-12.8 0-17.5L492.1 112.1c-4.8-4.5-12.4-4.3-17 .5L431.6 159c-4.6 4.9-4.3 12.7.8 17.2L523 256l-90.6 79.7c-5.1 4.5-5.5 12.3-.8 17.2l43.5 46.4c4.5 4.9 12.1 5.1 17 .6z"></path></svg>',
   });
 };
