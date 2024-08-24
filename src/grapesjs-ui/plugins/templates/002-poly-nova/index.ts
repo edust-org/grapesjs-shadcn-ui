0cb56bc80f45388d75f84803c969b82b2cc14996
dreammehedi <dreammehedihassan@gmail.com>
2024-08-24 07:36:12 +0600

    Update

--- src/grapesjs-ui/plugins/templates/002-poly-nova/index.ts
+++ src/grapesjs-ui/plugins/templates/002-poly-nova/index.ts
@@ -224,7 +224,7 @@ export const polyNova002 = {
         </div>
       </section>
 
-      <section>
+      <section class="w-full h-[500px]">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117658.79381577666!2d89.44986410388584!3d22.845507951947837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff9071cb47152f%3A0xf04b212290718952!2sKhulna!5e0!3m2!1sen!2sbd!4v1724408405444!5m2!1sen!2sbd"
           width="100%"
