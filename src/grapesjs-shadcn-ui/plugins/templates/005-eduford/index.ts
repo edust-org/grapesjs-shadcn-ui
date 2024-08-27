07f237fc36c32d02bd482ad9c57956d9f60e4c2d
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-08-27 10:32:26 +0600

    New templete added called Eduford

---
+++ src/grapesjs-shadcn-ui/plugins/templates/005-eduford/index.ts
@@ -0,0 +1,371 @@
+import thumbnail from "./eduford.avif";
+
+/**
+ * Nazmul Hossain
+ * GitHub Profile: https://github.com/NazmulHossain2905
+ * Template id: 005
+ */
+
+export const eduford005 = {
+  id: "005",
+  content: `
+  <header
+      id="home"
+      class="w-full min-h-screen relative bg-cover bg-center"
+      style="
+        background-image: linear-gradient(
+            rgba(4, 9, 30, 0.7),
+            rgba(4, 9, 30, 0.7)
+          ),
+          url('https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/banner.png');
+      "
+    >
+      <nav
+        class="max-w-[1400px] flex justify-between items-center mx-auto py-5 px-4"
+      >
+        <a href="#">
+          <img
+            class="w-[150px]"
+            src="https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/logo.png"
+            alt="Logo"
+          />
+        </a>
+
+        <ul class="sm:hidden md:flex text-white uppercase gap-5 text-sm">
+          <li>
+            <a
+              class="after:w-0 after:h-[2px] after:bg-red-500 after:block after:m-auto after:transition-[2s] hover:after:w-full"
+              href="#home"
+              >Home</a
+            >
+          </li>
+          <li>
+            <a
+              class="after:w-0 after:h-[2px] after:bg-red-500 after:block after:m-auto after:transition-[2s] hover:after:w-full"
+              href="#about"
+              >About</a
+            >
+          </li>
+          <li>
+            <a
+              class="after:w-0 after:h-[2px] after:bg-red-500 after:block after:m-auto after:transition-[2s] hover:after:w-full"
+              href="#course"
+              >Course</a
+            >
+          </li>
+          <li>
+            <a
+              class="after:w-0 after:h-[2px] after:bg-red-500 after:block after:m-auto after:transition-[2s] hover:after:w-full"
+              href="#blog"
+              >Blog</a
+            >
+          </li>
+          <li>
+            <a
+              class="after:w-0 after:h-[2px] after:bg-red-500 after:block after:m-auto after:transition-[2s] hover:after:w-full"
+              href="#contact"
+              >Contact</a
+            >
+          </li>
+        </ul>
+      </nav>
+
+      <div
+        class="w-[90%] mx-auto text-white text-center absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2"
+      >
+        <h1 class="text-[62px] font-bold">Shamsul Huda Khan College</h1>
+        <p class="mt-3 mb-10 p-2">
+          Shamsul Huda Khan College Placed at Biddadharpur Union, Moheshpur
+          Upozila, Jhenaidah District.
+        </p>
+        <button
+          class="border border-white text-[13px] bg-transparent cursor-pointer py-3 px-[34px] hover:transition-colors hover:bg-red-500"
+        >
+          Visit Us To Know More
+        </button>
+      </div>
+    </header>
+
+    <section id="course" class="max-w-[1300px] mx-auto py-16">
+      <div class="text-center mx-5 mb-16 mt-5">
+        <h3 class="text-4xl font-bold text-gray-700">Courses we offer</h3>
+        <p class="text-gray-500 leading-[22px] p-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          cum molestiae minima! Iure.
+        </p>
+      </div>
+
+      <div class="flex gap-10 flex-wrap px-5 md:px-0">
+        <div
+          class="flex-1 min-w-[400px] bg-red-50 rounded-md p-7 text-center transition-shadow hover:shadow-xl"
+        >
+          <h3 class="text-2xl font-bold mb-6">Intermediate</h3>
+          <p class="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Accusantium, eius sapiente consequuntur sed labore laborum
+            perferendis a impedit doloribus quidem.
+          </p>
+        </div>
+        <div
+          class="flex-1 min-w-[400px] bg-red-50 rounded-md p-7 text-center transition-shadow hover:shadow-xl"
+        >
+          <h3 class="text-2xl font-bold mb-6">Degree</h3>
+          <p class="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Accusantium, eius sapiente consequuntur sed labore laborum
+            perferendis a impedit doloribus quidem.
+          </p>
+        </div>
+        <div
+          class="flex-1 min-w-[400px] bg-red-50 rounded-md p-7 text-center transition-shadow hover:shadow-xl"
+        >
+          <h3 class="text-2xl font-bold mb-6">Post Graduation</h3>
+          <p class="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Accusantium, eius sapiente consequuntur sed labore laborum
+            perferendis a impedit doloribus quidem.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="max-w-[1300px] mx-auto py-16">
+      <div class="text-center mx-5 mb-16">
+        <h3 class="text-4xl font-bold text-gray-700">Our Global Campus</h3>
+        <p class="text-gray-500 leading-[22px] p-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          praesentium neque blanditiis.
+        </p>
+      </div>
+
+      <div class="flex justify-center gap-7 flex-wrap px-5 md:px-0">
+        <div
+          class="group flex-1 basis-[30%] relative overflow-hidden rounded-xl"
+        >
+          <img
+            class="w-full block"
+            src="https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/london.png"
+            alt="London"
+          />
+
+          <div
+            class="absolute w-full h-full bg-transparent top-0 left-0 transition duration-700 hover:bg-red-600 hover:bg-opacity-80"
+          >
+            <h3
+              class="text-2xl font-medium text-white group-hover:transition duration-1000 opacity-0 absolute w-full text-center left-[50%] -translate-x-1/2 bottom-0 group-hover:opacity-100 group-hover:bottom-[49%]"
+            >
+              LONDON
+            </h3>
+          </div>
+        </div>
+        <div
+          class="group flex-1 basis-[30%] relative overflow-hidden rounded-xl"
+        >
+          <img
+            class="w-full block"
+            src="https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/newyork.png"
+            alt="New York"
+          />
+
+          <div
+            class="absolute w-full h-full bg-transparent top-0 left-0 transition duration-700 hover:bg-red-600 hover:bg-opacity-80"
+          >
+            <h3
+              class="text-2xl font-medium text-white group-hover:transition duration-1000 opacity-0 absolute w-full text-center left-[50%] -translate-x-1/2 bottom-0 group-hover:opacity-100 group-hover:bottom-[49%]"
+            >
+              NEW YORK
+            </h3>
+          </div>
+        </div>
+        <div
+          class="group flex-1 basis-[30%] relative overflow-hidden rounded-xl"
+        >
+          <img
+            class="w-full block"
+            src="https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/washington.png"
+            alt="washington"
+          />
+
+          <div
+            class="absolute w-full h-full bg-transparent top-0 left-0 transition duration-700 hover:bg-red-600 hover:bg-opacity-80"
+          >
+            <h3
+              class="text-2xl uppercase font-medium text-white group-hover:transition duration-1000 opacity-0 absolute w-full text-center left-[50%] -translate-x-1/2 bottom-0 group-hover:opacity-100 group-hover:bottom-[49%]"
+            >
+              washington
+            </h3>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="max-w-[1300px] mx-auto py-16">
+      <div class="text-center mx-5 mb-16">
+        <h3 class="text-4xl font-bold text-gray-700">Our Facilities</h3>
+        <p class="text-gray-500 leading-[22px] p-3">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum
+          tempora necessitatibus quas?
+        </p>
+      </div>
+
+      <div class="flex justify-center gap-10 flex-wrap px-5 md:px-0">
+        <div class="flex-1 basis-[30%] rounded-md">
+          <img
+            class="w-full rounded-lg"
+            src="https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/library.png"
+            alt="library"
+          />
+
+          <h3 class="my-4 text-xl font-semibold">World Class Library</h3>
+          <p class="text-gray-500 leading-[22px]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Architecto, eos.
+          </p>
+        </div>
+        <div class="flex-1 basis-[30%] rounded-md">
+          <img
+            class="w-full rounded-lg"
+            src="https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/basketball.png"
+            alt="basketball"
+          />
+
+          <h3 class="my-4 text-xl font-semibold">Largest Playground</h3>
+          <p class="text-gray-500 leading-[22px]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Architecto, eos.
+          </p>
+        </div>
+        <div class="flex-1 basis-[30%] rounded-md">
+          <img
+            class="w-full rounded-lg"
+            src="https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/cafeteria.png"
+            alt="cafeteria"
+          />
+
+          <h3 class="my-4 text-xl font-semibold">Tasty and Healthy Food</h3>
+          <p class="text-gray-500 leading-[22px]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Architecto, eos.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section
+      id="contact"
+      class="max-w-[80%] mx-auto text-center my-14 md:my-[100px] p-12 md:p-[100px] rounded-xl w-full bg-cover bg-center"
+      style="
+        background-image: linear-gradient(
+            rgba(4, 9, 30, 0.7),
+            rgba(4, 9, 30, 0.7)
+          ),
+          url('https://raw.githubusercontent.com/NazmulHossain2905/eduford-landing-page/main/images/banner2.jpg');
+      "
+    >
+      <h1 class="text-xl md:text-3xl text-white font-bold mb-10">
+        Enroll For Our Various Online Courses <br />
+        Anywhere From The World
+      </h1>
+      <button
+        class="border border-white text-white uppercase text-[13px] bg-transparent cursor-pointer py-3 px-[34px] hover:transition-colors hover:bg-red-500"
+      >
+        Contact Us
+      </button>
+    </section>
+
+    <footer id="about" class="max-w-[1300px] mx-auto py-10">
+      <div class="text-center mx-5 mb-16">
+        <h3 class="text-xl font-bold text-gray-700 mb-[25px]">About US</h3>
+        <p class="text-gray-500 leading-[22px]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente
+          suscipit eos corrupti possimus minus <br />
+          fugit placeat consequuntur doloribus, laboriosam qui.
+        </p>
+      </div>
+      <div class="flex gap-5 text-red-600 justify-center">
+        <a href="#">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 320 512"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+            ></path>
+          </svg>
+        </a>
+        <a href="#">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 448 512"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+            ></path>
+          </svg>
+        </a>
+        <a href="#">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 512 512"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+            ></path>
+          </svg>
+        </a>
+        <a href="#">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 448 512"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+            ></path>
+          </svg>
+        </a>
+      </div>
+
+      <div
+        class="flex items-center justify-center mt-5 gap-1 text-gray-600 font-medium"
+      >
+        Made with
+        <svg
+          class="text-red-600"
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 512 512"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M256 448l-30.164-27.211C118.718 322.442 48 258.61 48 179.095 48 114.221 97.918 64 162.4 64c36.399 0 70.717 16.742 93.6 43.947C278.882 80.742 313.199 64 349.6 64 414.082 64 464 114.221 464 179.095c0 79.516-70.719 143.348-177.836 241.694L256 448z"
+          ></path>
+        </svg>
+        by Nazmul Hossain
+      </div>
+    </footer>
+  `,
+  thumbnail,
+};
